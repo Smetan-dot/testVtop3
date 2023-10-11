d547cb95e3f94b84a416dea36fb82c7b8584314d
Smetan-dot <smetanaandrei@gmail.com>
2023-10-11 22:01:32 +0300

    feat: add burger and header for 320px

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+function changeLang(){
     const button = document.querySelectorAll('.buttons-item')[0];
-
-    const span = document.createElement('span');
-    span.textContent = 'EN';
-    button.appendChild(span);
+    const span = document.getElementById('lang-span');
   
     button.addEventListener('click', () => {
         if(span.textContent === 'EN') span.textContent = 'RU'
         else span.textContent = 'EN';
     })
+}
 
+changeLang();
 
+function activateBurger(){
+    const burger = document.querySelector('.burger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        navMenu.classList.toggle('visible');
+    })
+}
+
+activateBurger();
